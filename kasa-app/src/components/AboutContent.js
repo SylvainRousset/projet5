afb70ec801151694273e68bc163ade3452dd9668
SylvainRousset <sylvaindebisca@hotmail.fr>
2024-09-17 16:24:24 +0200

    Add About page with AboutContent component

---
+++ kasa-app/src/components/AboutContent.js
@@ -0,0 +1,40 @@
+// src/components/AboutContent.jsx
+import React from 'react';
+import Banner from '../components/Banner';
+import Collapse from './Collapse';
+import './AboutContent.scss';
+import bannerImage from '../assets/images/backgrounds/background_about.png';
+
+const AboutContent = () => {
+  const categories = [
+    { 
+      title: 'Fiabilité', 
+      content: 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.' 
+    },
+    { 
+      title: 'Respect', 
+      content: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.' 
+    },
+    { 
+      title: 'Service', 
+      content: 'Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N\'hésitez pas à nous contacter si vous avez la moindre question.' 
+    },
+    { 
+      title: 'Sécurité', 
+      content: 'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l\'hôte qu\'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.' 
+    }
+  ];
+
+  return (
+    <div className="about-content">
+      <Banner image={bannerImage} overlayOpacity={0}/>
+      <div className="about-content__categories">
+        {categories.map((category, index) => (
+          <Collapse key={index} title={category.title} content={category.content} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AboutContent;
---
+++ kasa-app/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutContent from '../components/AboutContent'; // Assurez-vous que le chemin d'importation est correct
+
+const About = () => {
+  return (
+    <div className="about-page">
+      <AboutContent />
+    </div>
+  );
+};
+
+export default About;
