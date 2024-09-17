6d3c2ff34b7f5dafee605d8d91d4b262581f0e82
SylvainRousset <sylvaindebisca@hotmail.fr>
2024-09-17 12:44:33 +0200

    Add SCSS styles for HomeContent component

--- kasa-app/src/components/HomeContent.js
+++ kasa-app/src/components/HomeContent.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Banner from '../components/Banner';
-import './HommeContent.scss';
+import properties from '../data/properties.json'; 
+import './HomeContent.scss';
+import Card from './Card';
 import bannerImage from '../assets/images/backgrounds/background-home.png';  
 
 const HomeContent = () => {
   return (
     <div className="home-content">
       <Banner image={bannerImage} text="Chez vous, partout et ailleurs" overlayOpacity={0.5}/>
-
+      <div className="home-content__grid">
+        {properties.map(property => (
+          <Card key={property.id} property={property} />
+        ))}
+      </div>
     </div>
   );
 };
